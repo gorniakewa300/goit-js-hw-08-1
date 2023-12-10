70251987602a637ded181d19936cdc362d207e62
gorniakewa300 <132227665+gorniakewa300@users.noreply.github.com>
2023-12-10 13:44:43 +0100

    change code js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,54 +1,33 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
 const gallery = {
   container: document.querySelector('.gallery'),
 };
 
-// ------------creating tags---------
+// --------------creating tags------------
 
 const arrayImgTag = galleryItems.map ( ({preview, original, description}) => {
-  return `<div class="gallery__item">
+  return `<li class="gallery__item">
             <a class="gallery__link" href="${original}">
-              <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-              />
+              <img class="gallery__image" src="${preview}"" alt="${description}" />
             </a>
-          </div>`
+          </li>`;
 });
 
 gallery.container.insertAdjacentHTML("beforeend", arrayImgTag.join(''));
 
-// -----------connecting a lightbox from the basicLightbox library-------------
+// -----connecting a lightbox from the SimpleLightbox library-------
 
-let instance = '';
-
-const onImgClick = (event) => {
-  const target = event.target;
-
-  event.preventDefault();
-  if (target.nodeName !== "IMG"){
-    return;
-  };
-  instance = basicLightbox.create(`<img src="${target.dataset.source}" >`,
-   {onShow: () => {window.addEventListener('keydown', onEscClick);},
-   onClose: () => {window.removeEventListener('keydown', onEscClick);}
-  }
-   );
-  instance.show();
-};
-
-
-const onEscClick = (event) => {
-  if (event.code === 'Escape') {
-    window.removeEventListener('keydown', onEscClick);
-    instance.close();
-  };
-};
+let galleryContainer = new SimpleLightbox('.gallery a', {
+  close: false, 
+  showCounter: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionClass: 'text__label',
+});
+console.log(galleryItems);
 
